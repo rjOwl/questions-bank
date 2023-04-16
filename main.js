5667497e87a49622b6b33d9bd869346197faeeeb
mostafa <moustafa.youssuf@gmail.com>
2023-04-16 05:10:11 +0200

    Added and updated the file

---
+++ main.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+require("./boot/dbConnection");
+
+
+const PORT = process.env.PORT || 3000
+const app = express();
+
+app.use(express.json());
+
+
+const questionsRouter = require('./routes/questions');
+const tagsRouter = require('./routes/tags');
+
+/*  Routes */
+app.use("/questions", questionsRouter)
+app.use("/tags", tagsRouter)
+
+
+/////////
+
+// Application Level Middleware to trap request Info
+app.use("/", (req, res) => {
+    console.log(`Application Level Middleware : { Time : ${new Date()} , Method : ${req.method} , URL : ${req.url}}`);
+    res.send("OK")
+})
+
+
+/* Start The HTTP Server */
+app.listen(PORT, (err) => {
+    if (err) console.log(err)
+    else console.log(`Server Started On Port : ${PORT}`)
+})
