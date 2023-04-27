5bccf0ff7f8bc1ecb732acb3945346929db292ac
mostafa <moustafa.youssuf@gmail.com>
2023-04-27 19:29:59 +0200

    Added the write endpoint path

--- swagger.js
+++ swagger.js
@@ -10,14 +10,21 @@ const options = {
       version: '1.0.0',
       description: 'Your API description',
     },
+    servers: [
+      {
+        url: `http://localhost:${PORT}/api/v1`,
+        description: 'Development server',
+      },
+    ],
   },
+  
   apis: ['./routes/*.js'],
 };
 
 const specs = swaggerJSDoc(options);
 
 console.log("\n")
-console.log('\x1b[32m%s\x1b[0m', '\u2714', `SWAGGER RUNNING:  http://localhost:${PORT}/api-docs/#/\n`)
+console.log('\x1b[32m%s\x1b[0m', '\u2714', `SWAGGER RUNNING:  http://localhost:${PORT}/api/v1/api-docs/#/\n`)
 module.exports= (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+    app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 };
