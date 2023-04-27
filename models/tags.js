d68f263d9925e38838ea88b1d4281f16993d65c5
mostafa <moustafa.youssuf@gmail.com>
2023-04-27 18:28:17 +0200

    Placed index on tag's name

--- models/tags.js
+++ models/tags.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const TagSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique : true},
+    name: {type: String, required: true, unique : true, index:true},
     ancestors_path: [{ type: String, required: true }],
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
