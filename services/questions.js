aa0d6a4658279048cc741bea08be7e5163e2cfbd
mostafa <moustafa.youssuf@gmail.com>
2023-04-27 18:31:01 +0200

    Cleaning commented code

--- services/questions.js
+++ services/questions.js
@@ -21,27 +21,35 @@ const getAllQuestionsService = async()=>{
         return result;
     }
 }
-
+const getAncestorsPath = async(tagName)=>{
+  const ancestors_path_list = await TagModel.find({
+    "$or": [
+      {"ancestors_path": tagName},
+      {"name": tagName}
+    ]
+  }).lean().then(tags => Array.from(tags, tag => tag.name))
+  return ancestors_path_list;
+}
 // logic
-const  findQuestinosInDescendantsByAnnotation = async(tagName)=> {
+const  findQuestinosInDescendantsByAnnotation = async(tagName )=> {
     // Find all tags that either have the specified tag name in their ancestors path
     // or have the same ID as the specified tag name
-    const ancestors_path_query = await TagModel.find({
+    const ancestors_path_list = await TagModel.find({
         "$or": [
           {"ancestors_path": tagName},
           {"name": tagName}
         ]
       }).lean().then(tags => Array.from(tags, tag => tag.name))
-
-    // Find all questions that have at least one annotation in the ancestors_path_query array
+// console.log(ancestors_path_list)
+    // Find all questions that have at least one annotation in the ancestors_path_list array
     const result = await QuestionModel.find({
         "annotations": {
-          "$in": ancestors_path_query
+          "$in": ancestors_path_list
         }
       }).lean().then(questions => Array.from(questions, question => question.name));
       console.log(result)
-        return {"Questions": result}
-  }
+    return {"Questions": result}
+}
   
 // export
 module.exports = {
